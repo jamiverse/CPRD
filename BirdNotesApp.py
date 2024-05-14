fa74a8abbab3a865f57ca2cae6455594da4b62f1
Rocamadourr <151071894+Rocamadourr@users.noreply.github.com>
2024-05-14 20:53:45 +0200

    Update BirdNotesApp.py

--- BirdNotesApp.py
+++ BirdNotesApp.py
@@ -266,6 +266,13 @@ class App(customtkinter.CTk):
 
 
     def display_smooth_amplitude_plot(self, file_path):
+
+        self.spectrogram_canvas.destroy()
+        self.spectrogram_canvas = customtkinter.CTkFrame(self, width=960, height=250, corner_radius=0, border_width=1, border_color="black")
+        self.spectrogram_canvas.grid(row=0, column=1, padx=(20, 10), pady=(20, 0), sticky="nsew")
+        self.spectrogram_canvas.grid_columnconfigure(1, weight=1)
+        self.spectrogram_canvas.update()
+        
         if file_path.endswith('.npy'):
             # Chargez les données à partir du fichier npy
             self.audio_data = np.load(file_path)
